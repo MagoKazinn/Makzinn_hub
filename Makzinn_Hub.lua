d2d861f81af8b0a1e2a2e1d00a4a65b2580cfd11
MagoKazinn <165216908+MagoKazinn@users.noreply.github.com>
2024-03-28 18:37:20 -0300

    Add files via upload

---
+++ Makzinn_Hub.lua
@@ -0,0 +1,5 @@
+local gameid = game.PlaceId
+--Blade ball
+if(gameid == 13772394625) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MagoKazinn/Makzinn_hub/main/makzinn_Hub"))()
+end
